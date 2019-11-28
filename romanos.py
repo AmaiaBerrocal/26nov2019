514e98bc8c31b3e55572b9c80f72cefc6d4e3ad5
Korlat <amaia.berrocal@gmail.com>
2019-11-29 00:31:31 +0100

    control de repeticiones, numeros decrecientes...

--- romanos.py
+++ romanos.py
@@ -1,4 +1,7 @@
 valores = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+valores5 = {'V':5, 'L':50, 'D':500}
+simbolosOrdenados = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+
 
 def romano_a_arabigo(numRomano):
     numArabigo = 0
@@ -6,22 +9,43 @@ def romano_a_arabigo(numRomano):
     ultimoCaracter = ''
 
     for letra in numRomano:
-        if letra == ultimoCaracter:
-            numRepes +=1
-        else:
-            numRepes = 1
-        
-        if numRepes > 3:
-            return 0
-                   
         if letra in valores:
             numArabigo += valores[letra]
+            if ultimoCaracter == '':
+                pass
+            elif valores[ultimoCaracter] > valores[letra]:
+                numRepes = 1
+            elif valores[ultimoCaracter] == valores[letra]:
+                numRepes += 1
+
+                if letra in valores5:
+                    return 0
+
+                if numRepes > 3:
+                    return 0
+            elif valores[ultimoCaracter] < valores[letra]:    
+                if numRepes > 1:
+                    return 0
+                
+                if ultimoCaracter in valores5:
+                    return 0
+
+                distancia = simbolosOrdenados.index(letra) - simbolosOrdenados.index(ultimoCaracter)
+                if distancia > 2:
+                        return 0
+
+                numArabigo -= valores[ultimoCaracter]*2
+                numRepes = 1
         else:
             return 0
 
         ultimoCaracter = letra
 
     return numArabigo
+
+def arabigo_a_romano(valor):
+    pass    
+                                          
 
 
 
--- romanos_test.py
+++ romanos_test.py
@@ -1,6 +1,7 @@
 import unittest
 from romanos import romano_a_arabigo
 
+
 class RomanNumberTest(unittest.TestCase):
 
     def test_symbols_romans(self):
@@ -17,13 +18,44 @@ class RomanNumberTest(unittest.TestCase):
         self.assertEqual(romano_a_arabigo('XVI'), 16)
         self.assertEqual(romano_a_arabigo('III'), 3)
 
-    def test_no_mas_de_tres_repeticiones(self):
+    def test_control_repeticiones(self):
         self.assertEqual(romano_a_arabigo('IIII'), 0)
         self.assertEqual(romano_a_arabigo('LXXIII'), 73)
+        self.assertEqual(romano_a_arabigo('VV'), 0)
        
+
     def test_numeros_decrecientes(self):
+        self.assertEqual(romano_a_arabigo('IX'), 9)
         self.assertEqual(romano_a_arabigo('CMXCIX'), 999)
+        
+    def test_restas_no_admiten_repeticiones(self):   
+        self.assertEqual(romano_a_arabigo('MIIX'), 0)
+
+    def test_las_restas_no_admiten_derivados_del_5(self):
+        self.assertEqual(romano_a_arabigo('VC'), 0)
+        
+    def test_restas_no_admiten_mas_de_dos_ordenes(self):
+        self.assertEqual(romano_a_arabigo('IC'), 0)
+        self.assertEqual(romano_a_arabigo('IL'), 0)
+        self.assertEqual(romano_a_arabigo('IV'), 4)
+        self.assertEqual(romano_a_arabigo('XL'), 40)
+        self.assertEqual(romano_a_arabigo('XC'), 90)
+        self.assertEqual(romano_a_arabigo('CD'), 400)
+        self.assertEqual(romano_a_arabigo('CM'), 900)
+
+    def numeros_mayores_de_3999(self):
+        self.assertEqual(arabigo_a_romano('(IV)'), 4000)
+        self.assertEqual(arabigo_a_romano('(VII)CMXXIII'), 7923)
+
+        
+
+class ArabicNumberTest(unittest.TestCase):
+    def test_arabic_a_roman(self):
+        self.assertEqual(arabigo_a_romano(2123), 'MMCXXIII')
+        self.assertEqual(arabigo_a_romano(2444), 'MMCDXLIV')
+        self.assertEqual(arabigo_a_romano(3555), 'MMMDLV')
+        self.assertEqual(arabigo_a_romano(1678), 'MDCLXXVIII')
+        self.assertEqual(arabigo_a_romano(2999), 'MMCMXCIX')
 
 if __name__ == '__main__':
     unittest.main()
-
